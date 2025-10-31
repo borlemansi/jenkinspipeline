c3bf3849379da4bdb490580d13ef249acdbf895b
Mansi borle <95200804+borlemansi@users.noreply.github.com>
2025-10-31 20:22:52 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class hello{
     public static void main(String args[]){
-        System.out.println("Hello Jenkins. Vivek here. Its 12:!5!");
+        System.out.println("Hello Jenkins. Mansi here. Its 31st october!");
     }
 }
